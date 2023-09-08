124405c64d84787d73e09b89b0e1e925b5b23316
123alien <138240512+Alien123-js@users.noreply.github.com>
2023-09-08 16:05:16 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+const age = 18;
+console.log(typeof age);
+
+const word ="hello";
+
+console.log(typeof word);
+
+var isallow = true;
+
+console.log(typeof isallow);
+
+let data = 50/0;
+console.log(data);
+
+let action = (50/0);
+console.log(typeof action);
+
