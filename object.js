a7aa05b3bade714fcea6903a20270c668f382b2a
Alien <138240512+Alien-Logic@users.noreply.github.com>
2023-12-28 00:06:01 +0530

    added an example of object.js

---
+++ object.js
@@ -0,0 +1,20 @@
+var car={
+    color: "Red",
+    Doors : 4,
+    seats : 5};
+console.log(car. Doors)
+
+
+
+
+
+var person ={
+    fname : "joshi",
+    lname : "amith",
+    fullname : function(){
+        return this.fname +" " + this.lname
+    }
+}
+console.log(person.fullname());
+
+
